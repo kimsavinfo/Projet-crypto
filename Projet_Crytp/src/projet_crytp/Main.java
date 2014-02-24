72bc21c2f7cfa022b916bb03b71902e576a677f2
nicolas <machut.nicolas@live.fr>
2014-02-24 12:42:18 +0100

    Creation of ceasar class and main class

---
+++ Projet_Crytp/src/projet_crytp/Main.java
@@ -0,0 +1,13 @@
+package projet_crytp;
+
+/**
+ * Created by nicolas on 24/02/14.
+ */
+public class Main {
+
+    public static void Main(String args[])
+    {
+        Ceasar test = new Ceasar();
+        test.Crypting("aa", 2);
+    }
+}
