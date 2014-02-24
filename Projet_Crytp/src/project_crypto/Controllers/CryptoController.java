c0f5ee1bc36dbd1e424cfa9d8e4cd179956ce282
kimsavinfo <kimsavinfo@gmail.com>
2014-02-24 12:47:11 +0100

    début MVC framework
    TO DO :
    - controller + vue d'exemple
    - upload (?)

---
+++ Projet_Crytp/src/project_crypto/Controllers/CryptoController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project_crypto.Controllers;
+
+import project_crypto.Views.MainView;
+
+/**
+ *
+ * @author kimsavinfo
+ */
+public class CryptoController 
+{
+    public static void main(String[] args) {
+        // Fic import
+        
+        // View exemple
+        MainView mainView = new MainView();
+    } 
+}
