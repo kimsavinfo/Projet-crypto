768bfd806bfd0e7e3d29d826956cf45acfdf4825
kimsavinfo <kimsavinfo@gmail.com>
2014-02-25 10:52:15 +0100

    ReadWriteTextFile
    maxSizeSmallFile

--- Projet_Crytp/src/Library/ReadWriteTextFile.java
+++ Projet_Crytp/src/Library/ReadWriteTextFile.java
@@ -1,11 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * According to javadoc, Files.readAllLines :
+ * Object Files is more appropriated for small files
+ * However, its implementation uses buffering anyway
+ * So it could be used for large files too...
+ * => Code with Buffer in case
  */
 
 package Library;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
@@ -16,10 +19,20 @@ import java.nio.charset.StandardCharsets;
 public class ReadWriteTextFile 
 {
     
+    public static void main(String[] args) throws IOException
+    {
+        // Create itself for test
+        ReadWriteTextFile text = new ReadWriteTextFile();
+        
+    }
+    
+    
+    
     /** ===================================================================
      * Properties
      */
     final static String m_inputFileName = "C:\\Temp\\testCrypto.txt";
     final static String m_outPutFileName = "C:\\Temp\\resultCrypto.txt";
     final static Charset m_encoding = StandardCharsets.UTF_8;
+    final static int m_maxSizeSmallFile = 5000; // en octet, small file's maximum size
 }
