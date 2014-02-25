f560f9e57c2afaa09f26432fed28d5ddda8bb1e6
kimsavinfo <kimsavinfo@gmail.com>
2014-02-25 10:35:10 +0100

    ReadWriteTextFile
    Properties ok

---
+++ Projet_Crytp/src/Library/ReadWriteTextFile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Library;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @author kimsavinfo
+ */
+public class ReadWriteTextFile 
+{
+    
+    /** ===================================================================
+     * Properties
+     */
+    final static String m_inputFileName = "C:\\Temp\\testCrypto.txt";
+    final static String m_outPutFileName = "C:\\Temp\\resultCrypto.txt";
+    final static Charset m_encoding = StandardCharsets.UTF_8;
+}
